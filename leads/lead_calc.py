57d7618e7dff6e963494cffbdb621c115b199d60
willxxy <90741489+willxxy@users.noreply.github.com>
2024-08-04 13:35:23 -0400

    Update lead_calc.py

--- leads/lead_calc.py
+++ leads/lead_calc.py
@@ -2,9 +2,8 @@ import numpy as np
 
 lead_I = np.array([...]) 
 lead_II = np.array([...])
-lead_III = np.array([...])
+lead_III = lead_II - lead_I
 
-# more theoretically correct
 aVR = -((I + II) / 2)
 aVL = (I - III) / 2
 aVF = (II + III) / 2
